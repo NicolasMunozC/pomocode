1906ce5a59c32ee15fb1aae8b25a5f70d3970e36
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-03-27 21:49:40 -0300

    feat(stage): add Stage component

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { TbBrain, TbCoffee } from 'react-icons/tb'
 
 type StageProps = {
     currentStage: string,
@@ -6,9 +7,24 @@ type StageProps = {
 
 export default function Stage({currentStage}: StageProps) {
 
+  const [currentTheme, setCurrentTheme] = useState('bg-redAlpha-100/[0.15] border-red-900')
+
+  useEffect(() => {
+    currentStage === 'Focus' && setCurrentTheme('bg-redAlpha-100/[0.15] border-red-900')
+    currentStage === 'Short Break' && setCurrentTheme('bg-greenAlpha-100/[0.15] border-green-900')
+    currentStage === 'Long Break' && setCurrentTheme('bg-blueAlpha-100/[0.15] border-blue-900')
+  }, [currentStage])
+
 
   return (
-    <div>{currentStage}</div>
+    <div className={`w-fit flex flexRow px-4 py-1 gap-2 mx-auto rounded-3xl border-2 ${currentTheme}`}>
+      {
+        currentStage === 'Focus'
+        ? <TbBrain className='text-2xl self-center' />
+        : <TbCoffee className='text-2xl self-center' />
+      }
+      <h2 className='text-xl capitalize'>{currentStage}</h2>
+    </div>
   )
 }
 
